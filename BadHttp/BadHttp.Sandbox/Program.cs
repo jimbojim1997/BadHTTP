8e44bafde5c64931205b4db2467214d402197c2d
James Duda <jimbojim1997@gmail.com>
2020-05-10 19:15:01 +0100

    Added two projects: BadHttp.Sandbox, BadHttp.Tests.

---
+++ BadHttp/BadHttp.Sandbox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BadHttp.Server;
+
+namespace BadHttp.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
